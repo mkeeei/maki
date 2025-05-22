cc41e9d15a4b186e4f2bb65676556aba13118d81
DESKTOP-M9QQVEB\1220AM <kaolyyn12ayamin20@gmail.com>
2025-05-22 14:31:12 +0900

    0522

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    int Hp=0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void DecreaseHp()
+    {
+        Hp --;
+    }
+}
---
+++ Assets/Scripts/UkiwaController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UkiwaController : MonoBehaviour
+
+{
+    GameObject player;
+    void Start()
+    {
+        this.player = GameObject.Find("Ukiwa");
+    }
+
+    void Update()
+    {
+        // フレーム毎に等速で落下させる
+        transform.Translate(0, -0.1f, 0);
+
+        // 画面外に出たらオブジェクトを破棄する
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        // 当たり判定
+        Vector3 p1 = transform.position;             // 矢の中心座標
+        Vector3 p2 = this.player.transform.position; // 猫の中心座標
+        Vector3 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+
+        if (d < r1 + r2)
+        {
+            // 監督スクリプトにプレイやと衝突したことを伝える。
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            // 衝突した場合は、猫も消す
+            Destroy(gameObject);
+            // Destroy(player);
+        }
+    }
+}
+
